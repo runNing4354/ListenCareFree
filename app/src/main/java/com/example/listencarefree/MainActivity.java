e68f6373c40c1dbd2a74093d9c7ec94f0f78c0cb
runNing4354 <xoals4354@naver.com>
2022-11-13 01:44:01 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/listencarefree/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.listencarefree;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+}
